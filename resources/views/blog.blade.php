568e51c1b0f0ff40fbdb59206587e1e33e9e553b
rider <myintthwaykhine127@gmail.com>
2021-11-26 17:50:41 +0630

    rename welcome file to blogs file and create blog view file

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>blog</title>
+</head>
+<body>
+    <h1><a href="/blog">First Blog</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit adipisci incidunt quas eaque, voluptas cumque illum a hic doloribus provident minus quam dolores porro sint quia, pariatur perferendis deserunt debitis ratione. Fugiat, illo, incidunt quaerat voluptatibus quam eius laborum labore iusto minima odit illum dignissimos corrupti praesentium vero a cumque.
+    </p>
+
+    <p><a href="/">back to home</a></p>
+</body>
+</html>
