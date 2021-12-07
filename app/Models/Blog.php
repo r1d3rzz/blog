a4e4545cdba3c5ef3d4611255341fd364ff96f88
rider <myintthwaykhine127@gmail.com>
2021-12-07 12:19:59 +0630

    using collect function (orderBy()) and sort by date in blogs folder files

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -3,24 +3,22 @@ namespace App\Models;
 
 use Illuminate\Support\Facades\File;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
-/*
 
-install composer package : composer require spatie/yaml-front-matter
-and use That Package and Change Object Type in BLogs Folder > ...
-
- */
 class Blog {
 
         public $title;
         public $slug;
         public $intro;
         public $body;
+        public $date;
 
-        public function __construct($title,$slug,$intro,$body) {
+        public function __construct($title,$slug,$intro,$body,$date) {
             $this->title = $title;
             $this->slug = $slug;
             $this->intro = $intro;
             $this->body = $body;
+            $this->body = $body;
+            $this->date = $date;
         }
 
         /*
@@ -31,47 +29,21 @@ class Blog {
 
             });
 
+            ->sortBy()
+
         */
 
         public static function all(){
-            //refractor style
 
-            $blogs = collect( File::files(resource_path("blogs")))->map(function($file){
-                $obj=YamlFrontMatter::parseFile($file);
-                return new Blog($obj->title,$obj->slug,$obj->intro,$obj->body());
-            });
-            //-----------------------------------------------------------------------------
-            // $files = File::files(resource_path("blogs"));
-
-            //collect()->mop();
-            // $blogs = collect($files)->map(function($file){
-            //     $obj=YamlFrontMatter::parseFile($file);
-            //     $blog = new Blog($obj->title,$obj->slug,$obj->intro,$obj->body());
-            //     return $blog;
-            // });
-            //------------------------------------------------------------------------------
-            //Using Array Map
-            // $blogs = array_map(function($file){
-            //     $obj=YamlFrontMatter::parseFile($file);
-            //     $blog = new Blog($obj->title,$obj->slug,$obj->intro,$obj->body());
-            //     return $blog;
-            // },$files);
-            return $blogs;
+            return collect( File::files(resource_path("blogs")))
+                ->map(function($file){
+                    $obj=YamlFrontMatter::parseFile($file);
+                    return new Blog($obj->title,$obj->slug,$obj->intro,$obj->body(),$obj->date);
+            })->sortByDesc('date');
         }
 
         public static function find($slug){
-            //collet whereFisrt first last ();
-
             $blogs = static::all();
             return $blogs->firstWhere('slug',$slug);
-
-
-            // $path = resource_path("blogs/$slug.html");
-            // if(!file_exists($path)){
-            //     return redirect('/');
-            // }
-            // return cache()->remember("posts.$slug",now()->addMinutes(3),function() use($path) {
-            //     return file_get_contents($path);
-            // });
         }
     }
